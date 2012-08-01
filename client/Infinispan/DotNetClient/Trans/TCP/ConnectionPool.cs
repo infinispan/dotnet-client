26b2398ed9ea7a0f37c96685d203320aa9f5df13
Sunimal Rathnayake <sunimalr@gmail.com>
2012-08-02 01:22:26 +0530

    started implementing connection pooling

---
+++ client/Infinispan/DotNetClient/Trans/TCP/ConnectionPool.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.Concurrent;
+using Infinispan.DotNetClient.Trans;
+using Infinispan.DotNetClient.Exceptions;
+
+namespace Infinispan.DotNetClient.Trans.TCP
+{
+    public class ConnectionPool
+    {
+        private static ConnectionPool instance=null;
+        private ConcurrentDictionary<String,Transport> transportCollection;
+        private ConnectionPool()
+        { 
+        }
+
+        public static ConnectionPool getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ConnectionPool();
+            }
+            return instance;
+        }
+
+        public void prepareConnectionPool(IPEndPoint addr)
+        { 
+            transportCollection.TryAdd(addr.Address.ToString()+":"+addr.Port,new TCPTransport(addr.Address,addr.Port));
+        }
+
+        public Transport borrowTransport(IPEndPoint addr)
+        {
+            try
+            {
+                Transport temp;
+                transportCollection.TryRemove(addr.Address.ToString() + ":" + addr.Port, out temp);
+                return temp;
+            }
+            catch (Exception e)
+            {
+                throw new TransportException("Unable to fetch a transport! Requested transport not available : "+e);
+            }
+        }
+
+        public void releaseTransport(Transport transport)
+        {
+            //try
+            //{
+            //    transportCollection.TryAdd(transport.getIpAddress().ToString() + ":" + transport.getServerPort(), transport);
+            //}
+            //catch (Exception e)
+            //{
+            //    throw new TransportException("Failed to release transport : " + e);
+            //}
+        }
+
+    }
+}
---
+++ client/Infinispan/DotNetClient/Trans/TCP/RequestBalancer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Infinispan.DotNetClient.Trans.TCP
+{
+    public class RequestBalancer
+    {
+         private static RequestBalancer instance=null;
+        private RequestBalancer()
+        { 
+        }
+
+        public static RequestBalancer getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RequestBalancer();
+            }
+            return instance;
+        }
+
+        public void setServers()
+        { }
+
+        public IPEndPoint nextServer()
+        {
+            return new IPEndPoint(IPAddress.Loopback, 11222);
+        }
+    }
+}
