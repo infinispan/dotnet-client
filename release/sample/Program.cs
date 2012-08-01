8ecad79a1a1be71383fa23530f1e7d20c3fa94c3
Sunimal Rathnayake <sunimalr@gmail.com>
2012-08-02 01:30:55 +0530

    sample app changed according to API changes

--- release/sample/Program.cs
+++ release/sample/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Infinispan.DotNetClient;
@@ -37,13 +36,16 @@ namespace InfinispanDotnetClientSample
             cache.put<String, String>("key 1", "germanium", 0, 0);
 
             //Get the value of entry with key "key 1"
-            Console.WriteLine("key 1 value : " + cache.get<String>("key 1"));
+            Console.WriteLine("key 1 value : " + cache.get<String, String>("key 1"));
 
             //Put if absent is used to add entries if they are not existing in the cache
             cache.putIfAbsent<String, String>("key 1", "trinitrotoluene", 0, 0);
             cache.putIfAbsent<String, String>("key 2", "formaldehyde", 0, 0);
             Console.WriteLine("key 1 value after PutIfAbsent: " + cache.get<String>("key 1"));
             Console.WriteLine("Key 2 value after PutIfAbsent: " + cache.get<String>("key 2"));
+
+            cache.replace<String, String>("key 1", "fluoride", 0, 0);
+            Console.WriteLine("key 1 value after replace: " + cache.get<String, String>("key 1"));  
 
             //Check whether a particular key exists
             Console.WriteLine("key 1 is exist ?: " + cache.containsKey("key 1"));
