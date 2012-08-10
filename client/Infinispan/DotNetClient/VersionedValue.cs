add078dfe9930b4e21ef6b832c03b2e4755ac025
Sunimal Rathnayake <sunimalr@gmail.com>
2012-08-10 20:29:36 +0530

    VersionedValue.Value() returns an Object

--- client/Infinispan/DotNetClient/VersionedValue.cs
+++ client/Infinispan/DotNetClient/VersionedValue.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Infinispan.DotNetClient.Util;
 
 namespace Infinispan.DotNetClient
 {
     public class VersionedValue
     {
         private byte[] value;
-
+        private Serializer serializer;
         private long ver;
 
         public VersionedValue(long ver, byte[] value)
         {
             this.ver = ver;
             this.value = value;
+            this.serializer = new DefaultSerializer();
+        }
+         
+
+        public Object getValue()
+        {
+            return serializer.deserialize(this.value);
+            
         }
 
-
-        public byte[] Value
+        public void Value(byte[] val)
         {
-            get { return this.value; }
-            set { this.value = value; }
+            this.value = val;
         }
 
 
