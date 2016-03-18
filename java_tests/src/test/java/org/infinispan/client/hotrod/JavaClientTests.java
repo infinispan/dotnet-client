d4727dfa2809522cac23fc7a07a40f553b8585a7
Vittorio Rigamonti <rigazilla@gmail.com>
2016-03-18 13:22:19 +0100

    HRCPP-243 update ispn version for test, update tests and build script

--- java_tests/src/test/java/org/infinispan/client/hotrod/JavaClientTests.java
+++ java_tests/src/test/java/org/infinispan/client/hotrod/JavaClientTests.java
@@ -1,5 +1,11 @@
 package org.infinispan.client.hotrod;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.testng.Assert.assertEquals;
 
 import org.infinispan.client.hotrod.BulkGetKeysDistTest;
@@ -10,7 +16,6 @@ import org.infinispan.client.hotrod.BulkGetSimpleTest;
 import org.infinispan.client.hotrod.ClientAsymmetricClusterTest;
 import org.infinispan.client.hotrod.CacheManagerStoppedTest;
 import org.infinispan.client.hotrod.DefaultExpirationTest;
-import org.infinispan.client.hotrod.ForceReturnValueTest;
 import org.infinispan.client.hotrod.ForceReturnValuesTest;
 import org.infinispan.client.hotrod.HotRodIntegrationTest;
 import org.infinispan.client.hotrod.HotRodServerStartStopTest;
@@ -21,15 +26,34 @@ import org.infinispan.client.hotrod.ServerRestartTest;
 import org.infinispan.client.hotrod.ServerShutdownTest;
 import org.infinispan.client.hotrod.SocketTimeoutErrorTest;
 import org.infinispan.client.hotrod.retry.ServerFailureRetryTest;
+
+import org.testng.IMethodSelector;
+import org.testng.IMethodSelectorContext;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
 import org.testng.TestNG;
 import org.testng.reporters.TextReporter;
 
 import java.util.logging.*;
 
-public class JavaClientTests {
+public class JavaClientTests implements IMethodSelector {
+	private final static String [] passOverTestList = {
+        "CacheManagerNotStartedTest.testPutAllAsync",
+	"CacheManagerNotStartedTest.testPutAsync",
+	"CacheManagerNotStartedTest.testReplaceAsync",
+	"CacheManagerNotStartedTest.testVersionedRemoveAsync",
+	"CacheManagerStoppedTest.testPutAllAsync",
+	"CacheManagerStoppedTest.testPutAsync",
+	"CacheManagerStoppedTest.testReplaceAsync",
+	"CacheManagerStoppedTest.testVersionedRemoveAsync"
+	};
+	
+   private final static HashSet<String> passOverTestSet = new HashSet<String>(Arrays.asList(passOverTestList));
+	
    public static void main(String[] args) {
       TestNG testng = new TestNG();
-      TextReporter tr = new TextReporter("Java Tests", 2);
+     testng.addMethodSelector("org.infinispan.client.hotrod.JavaClientTests", 1);
+     TextReporter tr = new TextReporter("Java Tests", 2);
 
       if (Boolean.parseBoolean(System.getProperty("VERBOSE_HOTROD_JAVA_TESTS"))) {
           Logger.getLogger("").setLevel(Level.ALL);
@@ -53,7 +77,6 @@ public class JavaClientTests {
             DefaultExpirationTest.class,
             CacheManagerStoppedTest.class,
             ForceReturnValuesTest.class, 
-            ForceReturnValueTest.class, 
             HotRodIntegrationTest.class,
             HotRodServerStartStopTest.class, 
             HotRodStatisticsTest.class, 
@@ -78,4 +101,21 @@ public class JavaClientTests {
       assertEquals(tr.getFailedTests().size(), expectedTestFailures.length);
       System.exit(0);
    }
+
+@Override
+public boolean includeMethod(IMethodSelectorContext context, ITestNGMethod method, boolean isTestMethod) {
+	String testName = method.getRealClass().getSimpleName()+"."+method.getMethodName();
+	if (passOverTestSet.contains(testName))
+	{
+		context.setStopped(true);
+		return false;
+	}
+	return true;
+}
+
+@Override
+public void setTestMethods(List<ITestNGMethod> testMethods) {
+	// TODO Auto-generated method stub
+	
+}
 }
