5fce47b7f2c37d377a4fed153884e325c901a279
Tristan Tarrant <tristan@infinispan.org>
2026-08-21 18:29:04 +0200

    Add HybridCache support

--- src/InfinispanCacheServiceCollectionExtensions.cs
+++ src/InfinispanCacheServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -11,6 +12,16 @@ namespace Infinispan.Hotrod
         {
             services.Configure(setupAction);
             services.TryAddSingleton<IDistributedCache, InfinispanDistributedCache>();
+            return services;
+        }
+
+        public static IServiceCollection AddInfinispanHybridCache(this IServiceCollection services, Action<InfinispanDistributedCacheOptions> setupAction, Action<HybridCacheOptions> hybridCacheSetup = null)
+        {
+            services.AddInfinispanCache(setupAction);
+            if (hybridCacheSetup != null)
+                services.AddHybridCache(hybridCacheSetup);
+            else
+                services.AddHybridCache();
             return services;
         }
     }
